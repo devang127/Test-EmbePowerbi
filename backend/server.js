161ac37ef3031a1c2e1543323f7ce5eaf484646c
devang127 <devangsawant127@gmail.com>
2024-11-07 15:02:18 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect('mongodb://127.0.0.1:27017/test')
+mongoose.connect('mongodb://localhost:27017/test')
 
 app.post('/register',(req, res)=>{
     EmployeeModel.create(req.body)
